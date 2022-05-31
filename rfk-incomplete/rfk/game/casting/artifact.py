aa5de5d1f0688fe1a569c4ea6f59a415524d118f
valentinabassskrivanelli <valswaonyou@gmail.com>
2022-05-31 16:23:54 -0300

    def

--- rfk-incomplete/rfk/game/casting/artifact.py
+++ rfk-incomplete/rfk/game/casting/artifact.py
@@ -1,4 +1,4 @@
-from os import CLD_CONTINUED
+
 from game.casting.actor import Actor
 
 # TODO: Implement the Artifact class here. Don't forget to inherit from Actor!
@@ -9,6 +9,7 @@ class Artifact(Actor):
         super().__init__()
         self.message = ""
 
-    def get_mesagge
-
-    def set_message
+    def get_mesagge(self):
+        return self.message
+    def set_message(self, message):
+        self.message = message
